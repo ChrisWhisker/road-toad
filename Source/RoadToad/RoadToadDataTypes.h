5bd76cc0e1910c89ff9ab21b7b6bb4baac6c77cf
ChrisWhisker <chrisworc@gmail.com>
2024-01-29 10:15:03 -0500

    Move life timer functions into subsystem

---
+++ Source/RoadToad/RoadToadDataTypes.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "RoadToadDataTypes.generated.h"
+
+// Possible reasons for toad death
+UENUM(BlueprintType)
+enum class ECauseOfDeath : uint8
+{
+	Car UMETA(DisplayName="Car"),
+	Water UMETA(DisplayName="Water"),
+	Timer UMETA(DisplayName="Timer"),
+};
